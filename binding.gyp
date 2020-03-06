214f922799073718ec4b0bb7837c595f51d3537e
fastjack <quisefit@wcentric.com>
2020-03-06 19:27:41 +0100

    Improved building and publishing

--- binding.gyp
+++ binding.gyp
@@ -81,17 +81,6 @@
         "-lkaldi-base",
       ],
       'defines': [ 'NAPI_DISABLE_CPP_EXCEPTIONS' ],
-    },
-    {
-      "target_name": "action_after_build",
-      "type": "none",
-      "dependencies": [ "<(module_name)" ],
-      "copies": [
-        {
-          "files": [ "<(PRODUCT_DIR)/<(module_name).node" ],
-          "destination": "<(module_path)"
-        }
-      ]
     }
   ]
 }
--- test.js
+++ test.js
@@ -1,83 +1,138 @@
-const Kaldi = require('./lib/')
+const Kaldi	= require('./lib/')
+const File	= require('fs')
+const Path	= require('path')
+const Wav	= require('wavefile').WaveFile;
+const Glob	= require('glob')
 
-function main() {
+const testsGlob = Path.resolve(process.cwd(), process.argv[2])
 
-	const r1 = new Kaldi.EndpointRule({
-		must_contain_nonsilence: true,
-		min_trailing_silence: 3.0,
-		max_relative_cost: 5.5,
-		min_utterance_length: 12.0
-	})
+async function main() {
 
-	console.log(r1)
+	Glob(testsGlob, async (err, files) => {
 
-	const r2 = new Kaldi.EndpointRule({
-		must_contain_nonsilence: true,
-		min_trailing_silence: 3.0,
-		max_relative_cost: 5.5,
-		min_utterance_length: 12.0
-	})
-
-	console.log(r2)
-
-	const r3 = new Kaldi.EndpointRule({
-		must_contain_nonsilence: true,
-		min_trailing_silence: 3.0,
-		max_relative_cost: 5.5,
-		min_utterance_length: 12.0
-	})
-
-	console.log(r3)
-
-	const r4 = new Kaldi.EndpointRule({
-		must_contain_nonsilence: true,
-		min_trailing_silence: 3.0,
-		max_relative_cost: 5.5,
-		min_utterance_length: 12.0
-	})
-
-	console.log(r4)
-
-	const r5 = new Kaldi.EndpointRule({
-		must_contain_nonsilence: true,
-		min_trailing_silence: 3.0,
-		max_relative_cost: 5.5,
-		min_utterance_length: 12.0
-	})
-
-	console.log(r5)
-
-	const m = new Kaldi.OnlineNNet3Model({
-		model 						: './resources/model/final.mdl',
-		graph 						: './resources/HCLG.fst',
-		words 						: './resources/words.txt',
-		mfcc_config 				: './resources/online/conf/mfcc.conf',
-		ivector_extraction_config 	: './resources/online/conf/ivector_extractor.conf',
-		global_cmvn_stats 			: './resources/online/ivector_extractor/global_cmvn.stats',
-		frame_subsampling_factor 	: 3,
-		acoustic_scale 				: 1.0,
-		frames_per_chunk 			: 20,
-		debug 						: true
-	})
-
-	console.log(m)
-
-	const d = new Kaldi.OnlineNNet3GrammarDecoder(m, {
-		beam: 12.0,
-		lattice_beam: 4.0,
-		max_active: 2500,
-		min_active: 200,
-		endpointing: {
-			silence_phones: '1:2:3:4:5:6:7:8:9:10:11:12:13:14:15',
-			rule1: r1,
-			rule2: r2,
-			rule3: r3,
-			rule4: r4,
-			rule5: r5
+		if ( err ) {
+			console.error(err)
+			process.exit(1)
 		}
-	})
 
-	console.log(d)
+		if ( files.length === 0 ) {
+			console.log('No file to test')
+			process.exit(0)
+		}
+
+		const r1 = new Kaldi.EndpointRule({
+			must_contain_nonsilence: true,
+			min_trailing_silence: 3.0,
+			max_relative_cost: 5.5,
+			min_utterance_length: 12.0
+		})
+
+		console.log(r1)
+
+		const r2 = new Kaldi.EndpointRule({
+			must_contain_nonsilence: true,
+			min_trailing_silence: 3.0,
+			max_relative_cost: 5.5,
+			min_utterance_length: 12.0
+		})
+
+		console.log(r2)
+
+		const r3 = new Kaldi.EndpointRule({
+			must_contain_nonsilence: true,
+			min_trailing_silence: 3.0,
+			max_relative_cost: 5.5,
+			min_utterance_length: 12.0
+		})
+
+		console.log(r3)
+
+		const r4 = new Kaldi.EndpointRule({
+			must_contain_nonsilence: true,
+			min_trailing_silence: 3.0,
+			max_relative_cost: 5.5,
+			min_utterance_length: 12.0
+		})
+
+		console.log(r4)
+
+		const r5 = new Kaldi.EndpointRule({
+			must_contain_nonsilence: true,
+			min_trailing_silence: 3.0,
+			max_relative_cost: 5.5,
+			min_utterance_length: 12.0
+		})
+
+		console.log(r5)
+
+		const m = new Kaldi.OnlineNNet3Model({
+			model 						: './resources/model/final.mdl',
+			graph 						: './resources/HCLG.fst',
+			words 						: './resources/words.txt',
+			mfcc_config 				: './resources/online/conf/mfcc.conf',
+			ivector_extraction_config 	: './resources/online/conf/ivector_extractor.conf',
+			global_cmvn_stats 			: './resources/online/ivector_extractor/global_cmvn.stats',
+			frame_subsampling_factor 	: 3,
+			acoustic_scale 				: 1.0,
+			frames_per_chunk 			: 20,
+			debug 						: true
+		})
+
+		console.log(m)
+
+		const d = new Kaldi.OnlineNNet3GrammarDecoder(m, {
+			beam: 12.0,
+			lattice_beam: 4.0,
+			max_active: 2500,
+			min_active: 200,
+			endpointing: {
+				silence_phones: '1:2:3:4:5:6:7:8:9:10:11:12:13:14:15',
+				rule1: r1,
+				rule2: r2,
+				rule3: r3,
+				rule4: r4,
+				rule5: r5
+			}
+		})
+
+		console.log(d)
+
+		for ( let i = 0 ; i < files.length ; i++ ) {
+
+			const file = files[i]
+			const intentName = file.split('_')[0].split('/').pop()
+
+			console.log('=====================================================')
+			console.log(intentName)
+			console.log(file)
+
+			const wavFile = new Wav(File.readFileSync(file))
+			wavFile.toSampleRate(16000, {method: "sinc"});
+			// wavFile.toBitDepth("16");
+			const samples = wavFile.getSamples(false, Float32Array)
+
+			let start = 0
+			const step = 256
+			const num = samples.length
+
+			while ( start < num ) {
+				const s = samples.slice(start, start + step)
+				start += step
+				const endpointDetected = d.pushChunk(16000.0, s.length, s)
+
+				if ( endpointDetected ) {
+					console.log('>>>>>>>>>>>>>>>>>>>> ENDPOINT')
+					break
+				}
+
+			}
+
+			const res = d.getResult()
+
+			console.dir(res, { depth: null })
+		}
+
+	})
 }
 
 main()
